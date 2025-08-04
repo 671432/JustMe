265386f259eec45b58070d2535224e82e2a35a79
Master Raxer <justinjustin.roland@gmail.com>
2025-08-04 20:23:22 +0200

    trying a quickfix to apply background colors on wrapped or iOS devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './styles/Layout.css';
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
 import { Projects } from "./pages/Projects";
